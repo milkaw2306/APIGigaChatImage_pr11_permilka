1a9e01363d524dc24bb7b1aec7e9a025e2663ae9
student-a502 <student-a502@STUD-RM21-A502>
2025-12-29 08:55:41 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using APIGigaChatImage_pr11_permilka.Models.Response;
+using Newtonsoft.Json;
+
+namespace APIGigaChatImage_pr11_permilka
+{
+    class Program
+    {
+        static string ClientId = "***";
+        static string AuthorizationKey = "***";
+        public static async Task<string> GetToken(string rqUID, string bearer)
+        {
+            string ReturnToken = null;
+            string Url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth";
+            using (HttpClientHandler Handler = new HttpClientHandler())
+            {
+                Handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
+                using (HttpClient Clien = new HttpClient(Handler))
+                {
+                    HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Post, Url);
+                    Request.Headers.Add("Accept", "application/json");
+                    Request.Headers.Add("RqUID", rqUID);
+                    Request.Headers.Add("Authorization", $"Bearer {bearer}");
+                    var Data = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("scope", "GIGACHAT_API_PERS")
+                    };
+                    Request.Content = new FormUrlEncodedContent(Data);
+                    HttpResponseMessage Response = await Clien.SendAsync(Request);
+                    if (Response.IsSuccessStatusCode)
+                    {
+                        string ResponseContent = await Response.Content.ReadAsStringAsync();
+                        ResponseToken Token = JsonConvert.DeserializeObject<ResponseToken>(ResponseContent);
+                        ReturnToken = Token.access_token;
+                    }
+                }
+            }
+            return ReturnToken;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
